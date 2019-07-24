7d586723c582301fb982f7aff49d41c0ecc5eda5
Andrew Wagner <845683+drewag@users.noreply.github.com>
2019-07-24 15:27:27 -0600

    [CI] Remove tvOS from CI

--- Tests/test_xcodebuild.sh
+++ Tests/test_xcodebuild.sh
@@ -9,14 +9,3 @@ sudo xcode-select --switch /Applications/$1.app/Contents/Developer
 xcodebuild -version
 xcodebuild build -scheme SwiftlierUIKit-Package \
   -sdk iphonesimulator -destination "$IOS_DEVICE" | xcpretty
-xcodebuild build -scheme SwiftlierUIKit-Package \
-  -sdk appletvsimulator -destination "$TVOS_DEVICE" | xcpretty
-
-if [ -n "$CODECOV_JOB" ]; then
-  xcodebuild test -enableCodeCoverage YES -scheme SwiftlierUIKit-Package \
-    -sdk macosx | xcpretty
-  bash <(curl -s https://codecov.io/bash)
-else
-  xcodebuild test -scheme SwiftlierUIKit-Package \
-    -sdk macosx | xcpretty
-fi
