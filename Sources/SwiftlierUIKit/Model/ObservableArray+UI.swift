8f155630ebc0241d06dd506b17d5cb46eb32cf9c
Andrew Wagner <845683+drewag@users.noreply.github.com>
2020-06-28 11:59:11 -0600

    [NF] Empty row count for observing arrays with tableview

--- Sources/SwiftlierUIKit/Model/ObservableArray+UI.swift
+++ Sources/SwiftlierUIKit/Model/ObservableArray+UI.swift
@@ -10,16 +10,36 @@ import UIKit
 import Swiftlier
 
 extension ObservableArray {
-    public func add(observer: AnyObject, forSection section: Int, in tableView: UITableView, withIndexOffset indexOffset: Int = 0) {
+    public func add(observer: AnyObject, forSection section: Int, in tableView: UITableView, withIndexOffset indexOffset: Int = 0, withEmptyRowCount emptyRowCount: Int = 0) {
         self.add(
             observer: observer,
-            onDidInsert: { _, index in
+            onDidInsert: { [unowned self] _, index in
                 let indexPath = IndexPath(item: index + indexOffset, section: section)
-                tableView.insertRows(at: [indexPath], with: .automatic)
+                if self.values.count == 1 && emptyRowCount != 0 {
+                    // Was Empty
+                    tableView.reloadRows(at: [indexPath], with: .automatic)
+                    let indexPaths = (1 ..< emptyRowCount).map({IndexPath(item: $0 + indexOffset, section: section)})
+                    if !indexPaths.isEmpty {
+                        tableView.deleteRows(at: indexPaths, with: .automatic)
+                    }
+                }
+                else {
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                }
             },
-            onDidRemove: { _, index in
+            onDidRemove: { [unowned self] _, index in
                 let indexPath = IndexPath(item: index + indexOffset, section: section)
-                tableView.deleteRows(at: [indexPath], with: .automatic)
+                if self.values.count == 0 && emptyRowCount != 0 {
+                    // Now empty
+                    tableView.reloadRows(at: [indexPath], with: .automatic)
+                    let indexPaths = (1 ..< emptyRowCount).map({IndexPath(item: $0 + indexOffset, section: section)})
+                    if !indexPaths.isEmpty {
+                        tableView.insertRows(at: indexPaths, with: .automatic)
+                    }
+                }
+                else {
+                    tableView.deleteRows(at: [indexPath], with: .automatic)
+                }
             },
             onDidRemoveAll: { _ in
                 tableView.reloadSections(IndexSet(integer: section), with: .automatic)
